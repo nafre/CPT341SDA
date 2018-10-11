d4a40fdcde3d97167bf1a343b303fce681319eb0
Chua Wei Zhe <chuazhe@gmail.com>
2018-10-12 01:18:18 +0800

    Added Unit Testing for delete and edit recipe

--- CoffeeMaker/CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ CoffeeMaker/CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -1,16 +1,17 @@
 package edu.ncsu.csc326.coffeemaker;
 
 import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
+import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
 import junit.framework.TestCase;
 
 /**
  * 
  * @author Sarah Heckman
  *
- * Unit tests for CoffeeMaker class.
+ *         Unit tests for CoffeeMaker class.
  */
 public class CoffeeMakerTest extends TestCase {
-	
+
 	private CoffeeMaker cm;
 	private Recipe r1;
 	private Recipe r2;
@@ -19,8 +20,8 @@ public class CoffeeMakerTest extends TestCase {
 
 	protected void setUp() throws Exception {
 		cm = new CoffeeMaker();
-		
-		//Set up for r1
+
+		// Set up for r1
 		r1 = new Recipe();
 		r1.setName("Coffee");
 		r1.setAmtChocolate("0");
@@ -28,8 +29,8 @@ public class CoffeeMakerTest extends TestCase {
 		r1.setAmtMilk("1");
 		r1.setAmtSugar("1");
 		r1.setPrice("50");
-		
-		//Set up for r2
+
+		// Set up for r2
 		r2 = new Recipe();
 		r2.setName("Mocha");
 		r2.setAmtChocolate("20");
@@ -37,8 +38,8 @@ public class CoffeeMakerTest extends TestCase {
 		r2.setAmtMilk("1");
 		r2.setAmtSugar("1");
 		r2.setPrice("75");
-		
-		//Set up for r3
+
+		// Set up for r3
 		r3 = new Recipe();
 		r3.setName("Latte");
 		r3.setAmtChocolate("0");
@@ -46,8 +47,8 @@ public class CoffeeMakerTest extends TestCase {
 		r3.setAmtMilk("3");
 		r3.setAmtSugar("1");
 		r3.setPrice("100");
-		
-		//Set up for r4
+
+		// Set up for r4
 		r4 = new Recipe();
 		r4.setName("Hot Chocolate");
 		r4.setAmtChocolate("4");
@@ -55,30 +56,61 @@ public class CoffeeMakerTest extends TestCase {
 		r4.setAmtMilk("1");
 		r4.setAmtSugar("1");
 		r4.setPrice("65");
-		
+
 		super.setUp();
 	}
-	
+
 	public void testAddInventory() {
 		try {
-			cm.addInventory("4","7","0","9");
+			cm.addInventory("4", "7", "0", "9");
 		} catch (InventoryException e) {
 			fail("InventoryException should not be thrown");
 		}
 	}
-	
+
 	public void testAddInventoryException() {
 		try {
 			cm.addInventory("4", "-1", "asdf", "3");
 			fail("InventoryException should be thrown");
 		} catch (InventoryException e) {
-			//success if thrown
+			// success if thrown
 		}
 	}
-	
+
 	public void testMakeCoffee() {
 		cm.addRecipe(r1);
 		assertEquals(25, cm.makeCoffee(0, 75));
 	}
 
+	public void testDeleteRecipe() {
+		cm.addRecipe(r1);
+		cm.deleteRecipe(0);
+		if (cm.getRecipes()[0] != null) {
+			fail();
+		}
+	}
+
+	public void testEditRecipe() throws RecipeException {
+
+		cm.addRecipe(r1);
+
+		Recipe newRecipe = new Recipe();
+		newRecipe.setPrice("1");
+		newRecipe.setAmtCoffee("2");
+		newRecipe.setAmtMilk("3");
+		newRecipe.setAmtSugar("4");
+		newRecipe.setAmtChocolate("5");
+
+		cm.editRecipe(0, newRecipe);
+		System.out.println(cm.getRecipes()[0].getPrice());
+
+		if (cm.getRecipes()[0].getPrice() != 1 || cm.getRecipes()[0].getAmtCoffee() != 2
+				|| cm.getRecipes()[0].getAmtMilk() != 3 || cm.getRecipes()[0].getAmtSugar() != 4
+				|| cm.getRecipes()[0].getAmtChocolate() != 5) {
+			fail();
+
+		}
+
+	}
+	
 }
