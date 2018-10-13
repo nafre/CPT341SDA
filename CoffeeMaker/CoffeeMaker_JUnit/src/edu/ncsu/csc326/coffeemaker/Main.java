e2b9fb63b07a458d41ad00cd482c8860a2ca4184
gheewei1608 <43561828+gheewei1608@users.noreply.github.com>
2018-10-13 18:58:01 +0800

    Delete Test Commit Comment

--- CoffeeMaker/CoffeeMaker_JUnit/src/edu/ncsu/csc326/coffeemaker/Main.java
+++ CoffeeMaker/CoffeeMaker_JUnit/src/edu/ncsu/csc326/coffeemaker/Main.java
@@ -21,7 +21,7 @@ public class Main {
      * main menu commands.
      */
     public static void mainMenu() {
-    	//test commit =)
+    	
         System.out.println("1. Add a recipe");
         System.out.println("2. Delete a recipe");
         System.out.println("3. Edit a recipe");
